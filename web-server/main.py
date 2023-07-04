55bb05ee384d004f28f17ccb66f4708f6348822a
LTAQUE22 <luistd.22@gmail.com>
2023-07-04 15:52:25 -0500

    update web-services FastApi html

--- web-server/main.py
+++ web-server/main.py
@@ -1,15 +1,34 @@
 import store
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app=FastAPI()
+
 
 @app.get('/')
 def get_list():
     return [1,2,3,4]
 
-@app.get('/contact')
+@app.get('/contact', response_class=HTMLResponse)
 def get_list():
-    return {'name':'Platzi'}
+    
+    return """
+    <html>
+        <head>
+            <title>Aws Solution Arquitect</title>
+        </head>
+        <body>
+            <h1>Yo Soy Inevitable</h1>
+       
+        <a href="https://www.sport.es/es/noticias/historico/hakuho-luchador-sumo-trofeos-anunciara-su-retirada-12118193"> 
+        <h2>Sumo</h2>
+        
+        </a>
+        
+         
+           </body>
+    </html>
+    """
 
 def run():
     store.get_categories()
