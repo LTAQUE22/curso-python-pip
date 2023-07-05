c3fd5b3a3ec5e58075a92aebd1327d110602a1e5
LTAQUE22 <luistd.22@gmail.com>
2023-07-05 15:11:59 -0500

    Upgrade Charts

--- app/charts.py
+++ app/charts.py
@@ -20,7 +20,7 @@ def generate_pie_chart(labels, values):
     fig , ax =plt.subplots()
     ax.pie(values,labels=labels)
     ax.axis('equal')
-    plt.savefig(f'pie.png')
+    plt.savefig(f'___pie.png')
     plt.close()
 
 if __name__=='__main__':
